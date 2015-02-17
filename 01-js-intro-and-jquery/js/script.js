70d2f14b509dae2e98364475453ab048fdeed86f
juanaquino77 <juanaquino77@gmail.com>
2015-02-17 16:45:28 -0300

    Topic 1. punto  6,7,8.
    Agregado el textbox y la funcionalidad del button.
    (cualquier semejanza con el programa de television es pura casualidad :)
    )

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function(){
 	$('.js-content').fadeIn( 'slow', function(){
-		$('.js-fadeIn').css('visibility', 'visible');
+	  $('.js-fadeIn').css('visibility', 'visible');
+	});
+	$('.js-button').click(function(){
+	  var jqxhr = $.ajax( 'http://bootcamp.aws.af.cm/welcome/yourname.html').done(function(response)
+	  {
+    	$('.js-fadeIn').append('<p>' + response.response + '</p>');
+    	});
+		/*var jqxhr = $.get('http://bootcamp.aws.af.cm/welcome/yourname.html', function(response){
+			$('.js-fadeIn').append('<p>' + response.response + '</p>');
+		});*/
 	});
 });
