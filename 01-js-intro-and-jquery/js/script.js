61160a228a6822961ac0d7be1d8e1fad9b176dfd
juanaquino77 <juanaquino77@gmail.com>
2015-02-17 17:03:37 -0300

    Topic 1. punto 9.
    section in red when a server error occurs

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -6,9 +6,9 @@ $(document).ready(function(){
 	  var jqxhr = $.ajax( 'http://bootcamp.aws.af.cm/welcome/yourname.html').done(function(response)
 	  {
     	$('.js-fadeIn').append('<p>' + response.response + '</p>');
-    	});
-		/*var jqxhr = $.get('http://bootcamp.aws.af.cm/welcome/yourname.html', function(response){
-			$('.js-fadeIn').append('<p>' + response.response + '</p>');
-		});*/
+    	})
+  		.fail(function() {
+    		$('.js-fadeIn').css('background-color', 'red');
+  		});
 	});
 });
