c7234058a747bdb56b7b9e155aa6132740b81787
juanaquino77 <juanaquino77@gmail.com>
2015-02-18 11:58:58 -0300

    Topic 1. punto 10
    funcion highlight agregada

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -5,7 +5,11 @@ $(document).ready(function(){
 	$('.js-button').click(function(){
 	  var jqxhr = $.ajax( 'http://bootcamp.aws.af.cm/welcome/yourname.html').done(function(response)
 	  {
+	  	response.response='Welcome Juan Aquino!';
     	$('.js-fadeIn').append('<p>' + response.response + '</p>');
+    	$('.js-highlight').click(function() {
+  			$('.js-fadeIn').toggle('highlight');
+			});
     	})
   		.fail(function() {
     		$('.js-fadeIn').css('background-color', 'red');
