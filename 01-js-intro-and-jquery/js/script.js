12e9c42e8183efdc45f3a28ae2b6b9ba2b9b4d20
juanaquino77 <juanaquino77@gmail.com>
2015-02-19 11:17:30 -0300

    Terminado topico 1
    Codigo validado por la W3C :)

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -11,7 +11,7 @@ $(document).ready(function(){
 	  	  for (var i = 1; i <= response.albums.items.length; i++) {
 	  	  	$('.items').append('<h3>' + response.albums.items[i].type + '</h3>');
 	  	  	$('.items').append( i + ') ' + response.albums.items[i].name);
-	  	  	$('.items').append('<p>' + '<img src="'+ response.albums.items[i].images[1].url +'" width="64px" height="64px">' + '</p>');
+	  	  	$('.items').append('<p>' + '<img src="'+ response.albums.items[i].images[1].url +'" width="64px" height="64px" />' + '</p>');
 	  	  	$('.items').append('Link: ' + '<a href= "' + response.albums.items[i].external_urls.spotify + '" target="_blank" >'+ response.albums.items[i].name);
 		  	$('.items h3').css('border-top', 'solid 1px gray');  	
 	  	  };
