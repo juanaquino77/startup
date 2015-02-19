0aae123e6cbf44b7275252bd193aeb020b5de636
juanaquino77 <juanaquino77@gmail.com>
2015-02-19 10:46:39 -0300

    Topic 1. punto 12
    Agregada busqueda por artista

--- 01-js-intro-and-jquery/js/script.js
+++ 01-js-intro-and-jquery/js/script.js
@@ -3,15 +3,18 @@ $(document).ready(function(){
 	  $('.js-fadeIn').css('visibility', 'visible');
 	});
 	$('.js-albums').click(function(){
-	  var jqxhr = $.get( 'https://api.spotify.com/v1/search?q=Rolling%20Stones&type=album')
+	  var url = 'https://api.spotify.com/v1/search?q=';
+	  var artist = $('.inputSearch').val();
+	  var urlNew = url + artist + '&type=album';
+	  var jqxhr = $.get(urlNew)
 	  .done(function(response){
-	  	for (var i = 1; i <= response.albums.items.length; i++) {
-	  	  $('.items').append('<h3>' + response.albums.items[i].type + '</h3>');
-	  	  $('.items').append( i + ') ' + response.albums.items[i].name);
-	  	  $('.items').append('<p>' + '<img src="'+ response.albums.items[i].images[1].url +'" width="64px" height="64px">' + '</p>');
-	  	  $('.items').append('Link: ' + '<a href= "' + response.albums.items[i].external_urls.spotify + '" target="_blank" >'+ response.albums.items[i].name);
-		  $('.items h3').css('border-top', 'solid 1px gray');  	
-	  	}
+	  	  for (var i = 1; i <= response.albums.items.length; i++) {
+	  	  	$('.items').append('<h3>' + response.albums.items[i].type + '</h3>');
+	  	  	$('.items').append( i + ') ' + response.albums.items[i].name);
+	  	  	$('.items').append('<p>' + '<img src="'+ response.albums.items[i].images[1].url +'" width="64px" height="64px">' + '</p>');
+	  	  	$('.items').append('Link: ' + '<a href= "' + response.albums.items[i].external_urls.spotify + '" target="_blank" >'+ response.albums.items[i].name);
+		  	$('.items h3').css('border-top', 'solid 1px gray');  	
+	  	  };
       })
   		.fail(function(response) {
     	  alert('servidor no responde');
